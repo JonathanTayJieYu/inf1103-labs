9454e8224a9b560ad3d2d8b0267c50bf5a3b0dbe
Joanthan <2601587@sit.singaporetech.edu.sg>
2026-09-06 01:29:08 +0800

    Third Activity

--- hello.py
+++ hello.py
@@ -3,21 +3,21 @@
 # print("My First Post!")
 # print("=============")
 
-username = "cool_creator"
-bio = "Fun Blogger"
-follower = 100
-print('Username :',username)
-print('Bio :',bio)
-print('Followers :',follower)
-
-
+# username = "cool_creator"
+# bio = "Fun Blogger"
 # follower = 100
+# print('Username :',username)
+# print('Bio :',bio)
+# print('Followers :',follower)
 
-# follower += 50
-# print('Day 1:', follower)
 
-# follower += 20
-# print('Day 2:',follower)
+follower = 100
 
-# follower -= 30
-# print('Day 3:',follower)
+follower += 50
+print('Day 1:', follower)
+
+follower += 20
+print('Day 2:',follower)
+
+follower -= 30
+print('Day 3:',follower)
