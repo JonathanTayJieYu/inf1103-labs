6211eda6fbd9dd1cea804706d213016bcb3b0f4e
Joanthan <2601587@sit.singaporetech.edu.sg>
2026-09-06 01:43:11 +0800

    Seocnd Activity

--- hello.py
+++ hello.py
@@ -1,14 +1,14 @@
-print("============")
-print("Welcome here")
-print("My First Post!")
-print("=============")
+# print("============")
+# print("Welcome here")
+# print("My First Post!")
+# print("=============")
 
-# username = "cool_creator"
-# bio = "Fun Blogger"
-# follower = 100
-# print('Username :',username)
-# print('Bio :',bio)
-# print('Followers :',follower)
+username = "cool_creator"
+bio = "Fun Blogger"
+follower = 100
+print('Username :',username)
+print('Bio :',bio)
+print('Followers :',follower)
 
 
 # follower = 100
