f611fcc12201632d3f26f77eec04f18a1fe4e5c7
Joanthan <2601587@sit.singaporetech.edu.sg>
2026-09-06 01:41:35 +0800

    Fifth Activity

--- hello.py
+++ hello.py
@@ -23,11 +23,16 @@
 # print('Day 3:',follower)
 
 username = input('Enter Username :')
-age = input('Enter age :')
+age = int(input('Enter age :'))
 category = input('Enter Content Category :')
 
 print("\nInstagram Profile")
 print('===================')
 print('Username :', username)
-print('Age : ',age)
+print('Age :',age)
 print('Category :',category)
+
+if age > 40 and category == "fun":
+    print("You are old what is fun for you??")
+    
+    
