4f7aad7ea7532373b6a55887b8b229cdef143b0a
Joanthan <2601587@sit.singaporetech.edu.sg>
2026-09-06 01:28:12 +0800

    Second Activity

--- hello.py
+++ hello.py
@@ -1,4 +1,23 @@
-print("============")
-print("Welcome here")
-print("My First Post!")
-print("=============")
+# print("============")
+# print("Welcome here")
+# print("My First Post!")
+# print("=============")
+
+username = "cool_creator"
+bio = "Fun Blogger"
+follower = 100
+print('Username :',username)
+print('Bio :',bio)
+print('Followers :',follower)
+
+
+# follower = 100
+
+# follower += 50
+# print('Day 1:', follower)
+
+# follower += 20
+# print('Day 2:',follower)
+
+# follower -= 30
+# print('Day 3:',follower)
