a7cd5fb764c2263bc38c4cb0b720f225d8a6fbe9
Joanthan <2601587@sit.singaporetech.edu.sg>
2026-09-11 15:09:49 +0800

    week 2 Lab auditor.py

---
+++ auditor.py
@@ -0,0 +1,21 @@
+inventory = 0
+failed_entries = 0
+total_unit_processed = 0
+
+while True:
+    Stock_value = input('enter a stock quantity: ')
+    
+    if Stock_value.isdigit():
+        inventory+=1
+        total_unit_processed += 1
+        if total_unit_processed >= 5:
+                    print("Alert Total unit process more than 500")
+                    print('Total Unit Processed', total_unit_processed)
+                    break
+    elif Stock_value=="quit":
+        print('Total Unit Processed', total_unit_processed,"\n", 
+              "Number of Failed/Rejected Entries :", failed_entries)
+        break
+    else:
+        print('Eror, either you have entered a negative number or a wrong input ')
+        failed_entries +=1
